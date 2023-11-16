29b82045a850349072d75e00da5ccfadf6f33f7c
dinarakurmanbek <86626782+dinarakurmanbek@users.noreply.github.com>
2023-11-16 16:49:33 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-// Define global variables to keep track of the stream and recorder
-var theStream;
-var theRecorder;
-var recordedChunks = [];
 
 // This function initializes user media
 function getUserMedia(options, successCallback, failureCallback) {
@@ -13,121 +9,6 @@ function getUserMedia(options, successCallback, failureCallback) {
   throw new Error('User Media API not supported.');
 }
 
-
-// This function is called to start the media stream and recording
-function getStream() {
-  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-    alert('User Media API not supported.');
-    return;
-  }
-
-  var constraints = { video: true, audio: true };
-  getUserMedia(constraints, function (stream) {
-    var mediaControl = document.querySelector('video');
-    
-    // Older browsers may not have srcObject
-    if ("srcObject" in mediaControl) {
-      mediaControl.srcObject = stream;
-    } else {
-      // Avoid using this in new browsers, as it is going away.
-      mediaControl.src = window.URL.createObjectURL(stream);
-    }
-    
-    theStream = stream;
-    setupRecorder(stream); // This is a new function to encapsulate the recorder setup
-  }, function (err) {
-    alert('Error: ' + err);
-  });
-}
-function setupRecorder(stream) {
-  try {
-    theRecorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
-    theRecorder.ondataavailable = function(event) {
-      if (event.data.size > 0) {
-        recordedChunks.push(event.data);
-      }
-    };
-    theRecorder.start(100); // Collect 100ms of data chunks
-  } catch (e) {
-    console.error('Exception while creating MediaRecorder:', e);
-    return;
-  }
-  console.log('MediaRecorder created');
-}
-
-// This new function retrieves the video from the cache and downloads it.
-async function downloadFromCache() {
-  const videoKey = 'my_recorded_video.webm';  // This should match the key used when saving the video.
-
-  if (!('caches' in window)) {
-    alert('Cache API not supported!');
-    return;
-  }
-
-  try {
-    const cache = await caches.open('video-cache');
-    const cachedResponse = await cache.match(videoKey);
-    
-    if (!cachedResponse || !cachedResponse.ok) {
-      throw new Error('No cached video found!');
-    }
-
-    const blob = await cachedResponse.blob();
-    const url = window.URL.createObjectURL(blob);
-    
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'downloaded_video.webm';
-    document.body.appendChild(a);
-    a.click();
-    
-    // Cleanup
-    window.URL.revokeObjectURL(url);
-    document.body.removeChild(a);
-  } catch (err) {
-    console.error('Failed to download video from cache:', err);
-    alert(`Error: ${err.message}`);
-  }
-}
-
-
-
-// Stops the recording and saves the video to cache
-function stopRecordingAndSaveToCache() {
-  console.log('Stopping recording and saving data');
-  theRecorder.stop();
-  theStream.getTracks().forEach(track => track.stop());
-
-  theRecorder.onstop = function() {
-    // Create a Blob from the recorded chunks
-    var blob = new Blob(recordedChunks, { type: 'video/webm' });
-    saveToCache(blob);
-  };
-}
-
-// Saves the recording Blob to the cache
-function saveToCache(blob) {
-  if ('caches' in window) {
-    const videoKey = 'my_recorded_video.webm';
-    const request = new Request(videoKey, { mode: 'no-cors' });
-    const response = new Response(blob);
-
-    caches.open('video-cache').then(cache => {
-      cache.put(request, response).then(() => {
-        console.log('Saved video to cache.');
-      }).catch(error => {
-        console.error('Failed to save video to cache:', error);
-      });
-    });
-  } else {
-    console.error('Cache API not supported');
-  }
-}
-
-
-// ... Rest of your code such as service worker registration and notifications ...
-
-
 var target = document.getElementById('target');
 var watchId;
 
@@ -138,81 +19,7 @@ function appendLocation(location, verb) {
   target.appendChild(newLocation);
 }
 
-if ('geolocation' in navigator) {
-  document.getElementById('askButton').addEventListener('click', function () {
-    navigator.geolocation.getCurrentPosition(function (location) {
-      appendLocation(location, 'fetched');
-    });
-    watchId = navigator.geolocation.watchPosition(appendLocation);
-  });
-} else {
-  target.innerText = 'Geolocation API not supported.';
-}
-if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) {
-  document.getElementById('moApi').innerHTML = 'Generic Sensor API';
-  
-  let lastReadingTimestamp;
-  let accelerometer = new LinearAccelerationSensor();
-  accelerometer.addEventListener('reading', e => {
-    if (lastReadingTimestamp) {
-      intervalHandler(Math.round(accelerometer.timestamp - lastReadingTimestamp));
-    }
-    lastReadingTimestamp = accelerometer.timestamp
-    accelerationHandler(accelerometer, 'moAccel');
-  });
-  accelerometer.start();
-  
-  if ('GravitySensor' in window) {
-    let gravity = new GravitySensor();
-    gravity.addEventListener('reading', e => accelerationHandler(gravity, 'moAccelGrav'));
-    gravity.start();
-  }
-  
-  let gyroscope = new Gyroscope();
-  gyroscope.addEventListener('reading', e => rotationHandler({
-    alpha: gyroscope.x,
-    beta: gyroscope.y,
-    gamma: gyroscope.z
-  }));
-  gyroscope.start();
-  
-} else if ('DeviceMotionEvent' in window) {
-  document.getElementById('moApi').innerHTML = 'Device Motion API';
-  
-  var onDeviceMotion = function (eventData) {
-    accelerationHandler(eventData.acceleration, 'moAccel');
-    accelerationHandler(eventData.accelerationIncludingGravity, 'moAccelGrav');
-    rotationHandler(eventData.rotationRate);
-    intervalHandler(eventData.interval);
-  }
-  
-  window.addEventListener('devicemotion', onDeviceMotion, false);
-} else {
-  document.getElementById('moApi').innerHTML = 'No Accelerometer & Gyroscope API available';
-}
-
-function accelerationHandler(acceleration, targetId) {
-  var info, xyz = "[X, Y, Z]";
-
-  info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
-  info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
-  info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
-  document.getElementById(targetId).innerHTML = info;
-}
-
-function rotationHandler(rotation) {
-  var info, xyz = "[X, Y, Z]";
-
-  info = xyz.replace("X", rotation.alpha && rotation.alpha.toFixed(3));
-  info = info.replace("Y", rotation.beta && rotation.beta.toFixed(3));
-  info = info.replace("Z", rotation.gamma && rotation.gamma.toFixed(3));
-  document.getElementById("moRotation").innerHTML = info;
-}
-
-function intervalHandler(interval) {
-  document.getElementById("moInterval").innerHTML = interval;
-}
-
+// storage quotas
 if ('storage' in navigator && 'estimate' in navigator.storage) {
   navigator.storage.estimate()
     .then(estimate => {
@@ -236,53 +43,9 @@ function requestPersistence() {
         document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
       });
   }
-
-if ('localStorage' in window || 'sessionStorage' in window) {
-  var selectedEngine;
-
-  var logTarget = document.getElementById('target');
-  var valueInput = document.getElementById('value');
-
-  var reloadInputValue = function () {
-  console.log(selectedEngine, window[selectedEngine].getItem('myKey'))
-    valueInput.value = window[selectedEngine].getItem('myKey') || '';
-  }
-  
-  var selectEngine = function (engine) {
-    selectedEngine = engine;
-    reloadInputValue();
-  };
-
-  function handleChange(change) {
-    var timeBadge = new Date().toTimeString().split(' ')[0];
-    var newState = document.createElement('p');
-    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
-    logTarget.appendChild(newState);
-  }
-
-  var radios = document.querySelectorAll('#selectEngine input');
-  for (var i = 0; i < radios.length; ++i) {
-    radios[i].addEventListener('change', function () {
-      selectEngine(this.value)
-    });
-  }
-  
-  selectEngine('localStorage');
-
-  valueInput.addEventListener('keyup', function () {
-    window[selectedEngine].setItem('myKey', this.value);
-  });
-
-  var onStorageChanged = function (change) {
-    var engine = change.storageArea === window.localStorage ? 'localStorage' : 'sessionStorage';
-    handleChange('External change in ' + engine + ': key ' + change.key + ' changed from ' + change.oldValue + ' to ' + change.newValue + '');
-    if (engine === selectedEngine) {
-      reloadInputValue();
-    }
-  }
-
-  window.addEventListener('storage', onStorageChanged);
 }
+
+//file access
 
 function getReadFile(reader, i) {
   return function () {
@@ -331,4 +94,53 @@ async function writeFile() {
   await writer.close()
   
   target.innerHTML = 'Test content written to ' + file.name + '.';
+}
+
+// offline storage
+
+if ('localStorage' in window || 'sessionStorage' in window) {
+  var selectedEngine;
+
+  var logTarget = document.getElementById('target');
+  var valueInput = document.getElementById('value');
+
+  var reloadInputValue = function () {
+  console.log(selectedEngine, window[selectedEngine].getItem('myKey'))
+    valueInput.value = window[selectedEngine].getItem('myKey') || '';
+  }
+  
+  var selectEngine = function (engine) {
+    selectedEngine = engine;
+    reloadInputValue();
+  };
+
+  function handleChange(change) {
+    var timeBadge = new Date().toTimeString().split(' ')[0];
+    var newState = document.createElement('p');
+    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
+    logTarget.appendChild(newState);
+  }
+
+  var radios = document.querySelectorAll('#selectEngine input');
+  for (var i = 0; i < radios.length; ++i) {
+    radios[i].addEventListener('change', function () {
+      selectEngine(this.value)
+    });
+  }
+  
+  selectEngine('localStorage');
+
+  valueInput.addEventListener('keyup', function () {
+    window[selectedEngine].setItem('myKey', this.value);
+  });
+
+  var onStorageChanged = function (change) {
+    var engine = change.storageArea === window.localStorage ? 'localStorage' : 'sessionStorage';
+    handleChange('External change in ' + engine + ': key ' + change.key + ' changed from ' + change.oldValue + ' to ' + change.newValue + '');
+    if (engine === selectedEngine) {
+      reloadInputValue();
+    }
+  }
+
+  window.addEventListener('storage', onStorageChanged);
 }
